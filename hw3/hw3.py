08ea6a3616005ec412ca24810e204c3271a1469d
Alex9823 <alexsalamatina@gmail.com>
2016-10-17 22:10:56 +0300

    Add files via upload

---
+++ hw3/hw3.py
@@ -0,0 +1,13 @@
+a=input('Choose number a')
+b=input('Choose number b')
+c=input('Choose number c')
+a=int(a)
+b=int(b)
+c=int(c)
+if a+b==c :
+    print('a+b=c')
+elif a/b==c:
+    print('a/b=c')
+else:
+    print('Mistake has occured')
+    
