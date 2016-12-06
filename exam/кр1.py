b464a0bf51df08cbb032425dee5eb73602d7ec01
Alex9823 <alexsalamatina@gmail.com>
2016-12-06 16:40:43 +0300

    Update кр1.py

--- exam/кр1.py
+++ exam/кр1.py
@@ -5,7 +5,7 @@ with open('freq1.txt','r', encoding = 'utf-8') as f:
         word =line.split('|')
         for i in word:
             if i.endswith('союз') or i.endswith('союз '):
-                print (word)
+                print (line)
     
     
  
